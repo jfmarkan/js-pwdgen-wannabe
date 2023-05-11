03145d0324ddc60a02e0df3b5dd17a70aebafb5c
jfmarkan <jfmarkan@gmail.com>
2023-05-11 14:24:28 +0200

    Task done

---
+++ js/script.js
@@ -0,0 +1,5 @@
+userName = prompt("Inseert your name");
+userSurname = prompt("Insert your surname");
+userColor = prompt("Insert your favourite colour");
+
+document.getElementById("password-generated").innerHTML = userName + userSurname + userColor + 21;
